c83f23aa4ab8025857d01ea78f7ec10f42d57a49
aja014 <142507287+aja014@users.noreply.github.com>
2024-03-25 15:58:13 +0800

    day 1

--- Snake/.cpp
+++ Snake/.cpp
@@ -1,56 +1,95 @@
-#include <Coorxy.h>
 #include <iostream>
+#include <Coorxy.h>
 #include <conio.h>
 
 using namespace std;
 
-void draw() {
-}
-int main() {
-	int x=1,y=1;
-	A:
+//int main() {
+//	// pointer
+//	// * is used to store the memory address of another variable
+//	// & is used to get the value address of its storage location
+//	int p = 1;
+//
+//	int *x = &p;
+//	int &y = p;
+//
+//	cout << *x;
+//
+//
+//	coorxy(0, 29); system("pause");
+//}
+
+int x = 45, y = 15;
+int xtail1, xtail2, xtail3, xtail4;
+
+void click() {
 	switch (_getch()) {
-	case 75: // left
+	case 72:
+		if (y <= 29) {
+			y--;
+			if (y < 0){
+				y = 29;
+			}
+		}
+		break;
+	case 75:
 		if (x <= 119) {
 			x--;
 			if (x < 0) {
 				x = 119;
 			}
 		}
-		coorxy(x, y); cout << char(223);
 		break;
-	case 72: // up
-		if (y <= 29) {
-			y--;
-			if (y < 0) {
-				y = 29;
-			}
-		}
-		coorxy(x, y); cout << char(223);
-		break;
-	case 80: // down
-		if (y <= 29) {
-			y++;
-			if (y > 29) {
-				y = 0;
-			}
-		}
-		coorxy(x, y); cout << char(223);
-		break;
-	case 77: // right
+	case 77:
 		if (x <= 119) {
 			x++;
 			if (x > 119) {
 				x = 0;
 			}
 		}
-		coorxy(x, y);cout << char(223);
 		break;
-	case 27:
-		exit(0);
+	case 80:
+		if (y <= 29) {
+			y++;
+			if (y > 29) {
+				y = 0;
+			}
+		}
 		break;
 	}
-	goto A;
+}
+
+void logic() {
+	system("cls");
+	xtail4 = x - 4;
+	/*tail2 = x - 2;
+	tail1 = x - 1;*/
+
+	xtail4 = xtail3;
+	xtail3 = xtail2;
+	xtail2 = xtail1;
+	xtail1 = x;
+
+
+
+	coorxy(xtail4, y); cout << "#";
+	coorxy(xtail3, y); cout << "#";
+	coorxy(xtail2, y); cout << "#";
+	coorxy(xtail1, y); cout << "#";
+
+
+
+
+}
+
+
+int main() {
+
+	while (true) {
+		click();
+		logic();
+	}
+
 
 
 
